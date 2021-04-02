60137a37f35476b81fe28b34c1baf80ffc7f86df
Jesus Alvarez Ruiz <jalvarezruiz@gmail.com>
2021-04-02 07:53:07 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,56 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # # . # #
+        . # . # .
+        . . . . .
+        . # # # .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showIcon(IconNames.Surprised)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(200)
+})
